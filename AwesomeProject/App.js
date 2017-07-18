a807d07204e11a0a5df4ae06c543bce27d5ab6be
magdasid <magda_si@wp.pl>
2017-07-18 16:55:01 +0200

    Dodanie nowych widoków

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
 
+/* class export allows importing and using this component by other */
 export default class App extends React.Component {
+  state = {
+    name: "Magda"
+  }
+  text() {
+      return <Text>Hello {this.state.name}</Text>
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <View style={styles.navBar}>
+          <Text style={styles.navBarButton}>Back</Text>
+          <Text style={styles.navBarHeader}>Awesome App</Text>
+          <Text style={styles.navBarButton}>More</Text>
+        </View>
+        <View style={styles.content}>
+          {this.text()}
+        </View>
       </View>
     );
   }
@@ -13,9 +31,29 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1, /* it will take full screen */
+    backgroundColor: '#fff'
+  },
+  navBar: {
+    flexDirection: 'row',
+    paddingTop: 30,
+    height: 64,
+    backgroundColor: '#033192'
+  },
+  navBarButton: {
+    color: '#FFFFFF',
+    textAlign:'center',
+    width: 64
+  },
+  navBarHeader: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
